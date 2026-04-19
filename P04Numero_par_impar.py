56538429eded2c3755495f4330aed99d73a00c46
bella0996 <isaortega996@gmail.com>
2026-04-18 18:10:34 -0600

    P04Numero_par_impar.py

--- P04Numero_par_impar.py
+++ P04Numero_par_impar.py
@@ -1,7 +1,6 @@
-# Pedir el número
+#Isabella Nahomy Robles Ortega
 num = int(input("Ingresa un número: "))
 
-# Evaluar si es par o impar
 if (num % 2 == 0 and not num % 2 != 0):
     print("El número es par")
 elif (num % 2 != 0 or not num % 2 == 0):

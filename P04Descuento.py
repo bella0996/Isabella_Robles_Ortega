66a41ee14b7c0bd2b871c7b2246ed7cbde05ae9e
bella0996 <isaortega996@gmail.com>
2026-04-18 18:09:34 -0600

    P04Descuento.py
    Refactor discount calculation and output total.

--- P04Descuento.py
+++ P04Descuento.py
@@ -1,9 +1,8 @@
-# Pedir datos
+#Isabella Nahomy Robles Ortega
 precio = float(input("Ingresa el precio del producto: "))
 es_estudiante = input("¿Eres estudiante? (si/no): ").lower()
 compra_mayor = input("¿Tu compra es mayor a 1000? (si/no): ").lower()
 
-# Aplicar descuento
 # Descuento si es estudiante Y la compra es mayor a 1000
 # O si NO es estudiante pero la compra es mayor a 2000
 if (es_estudiante == "si" and compra_mayor == "si") or (not es_estudiante == "si" and precio > 2000):
